b11bc82e5a5db97172425205eb2934d7f1584f37
Jenny Taylor <jennytaylor82@gmail.com>
2022-02-09 16:34:05 +0000

    completed game functionality

--- tic-tac-toe-app/src/Components/Game/index.js
+++ tic-tac-toe-app/src/Components/Game/index.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
-import Board from '../Board';
+import Board from "../Board";
 
-import './Game.css';
+import "./Game.css";
 
 function Game() {
   const [board, setBoard] = useState([
@@ -18,25 +18,62 @@ function Game() {
   ]);
 
   const [xTurn, setXTurn] = useState(true);
+  const [winner, setWinner] = useState('')
 
-  function playerMove(e) {
+  useEffect(() => {
+  checkIfWinner(board)
+
+}, [board])
+
+  function checkIfWinner(board) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        setWinner(board[a])
+        return board[a]
+      }
+      if(!board.includes(null)){setWinner("draw")}
+    }
     
-    let index = e.target.id;
-   
-    let symbol;
-    if (xTurn === true) { symbol = "X" } else { symbol = "O" }
- 
-    if (e.target.value === undefined) { setBoard([...board.slice(0, index), symbol, ...board.slice(index+1)]) };
-
-    setXTurn(!xTurn);
+    return null;
   }
 
+  function playerMove(e) {
+    if(winner){return}
+    if (board.includes(null)) {
+      let index = Number(e.target.id);
+
+      let symbol;
+      if (xTurn === true) {
+        symbol = "X";
+      } else {
+        symbol = "O";
+      }
+
+      if (e.target.innerText === "") {
+        setBoard([...board.slice(0, index), symbol, ...board.slice(index + 1)]);
+      }
+
+      setXTurn(!xTurn);
+    } 
+  }
 
   return (
     <div className="Game">
       <Board board={board} clickHandler={playerMove} />
-      <h2>Player Turn</h2>
-      <h2>Winner</h2>
+      <h2>Player Turn: {xTurn ? "X": "O" }</h2>
+      {winner && <h2>{winner === "draw" ? "It's a draw" : `The winner is ${winner}`}</h2>}
     </div>
   );
 }
