e75e6b1fa52481471acc5efe315a1593cd589b4d
Sevi <correosevi@gmail.com>
2022-02-09 16:54:16 +0000

    Applied styles to board

--- tic-tac-toe-app/src/Components/Game/index.js
+++ tic-tac-toe-app/src/Components/Game/index.js
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import Board from "../Board";
+import Board from '../Board';
 
-import "./Game.css";
+import './Game.css';
 
 function Game() {
   const [board, setBoard] = useState([
@@ -18,12 +18,11 @@ function Game() {
   ]);
 
   const [xTurn, setXTurn] = useState(true);
-  const [winner, setWinner] = useState('')
+  const [winner, setWinner] = useState('');
 
   useEffect(() => {
-  checkIfWinner(board)
-
-}, [board])
+    checkIfWinner(board);
+  }, [board]);
 
   function checkIfWinner(board) {
     const lines = [
@@ -40,40 +39,48 @@ function Game() {
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-        setWinner(board[a])
-        return board[a]
+        setWinner(board[a]);
+        return board[a];
       }
-      if(!board.includes(null)){setWinner("draw")}
+      if (!board.includes(null)) {
+        setWinner('draw');
+      }
     }
-    
+
     return null;
   }
 
   function playerMove(e) {
-    if(winner){return}
+    if (winner) {
+      return;
+    }
     if (board.includes(null)) {
       let index = Number(e.target.id);
 
       let symbol;
       if (xTurn === true) {
-        symbol = "X";
+        symbol = 'X';
       } else {
-        symbol = "O";
+        symbol = 'O';
       }
 
-      if (e.target.innerText === "") {
+      if (e.target.innerText === '') {
         setBoard([...board.slice(0, index), symbol, ...board.slice(index + 1)]);
       }
 
       setXTurn(!xTurn);
-    } 
+    }
   }
 
   return (
     <div className="Game">
       <Board board={board} clickHandler={playerMove} />
-      <h2>Player Turn: {xTurn ? "X": "O" }</h2>
-      {winner && <h2>{winner === "draw" ? "It's a draw" : `The winner is ${winner}`}</h2>}
+      <h2>Player Turn: {xTurn ? 'X' : 'O'}</h2>
+
+      {winner && winner === 'draw' && <h2 className="draw">It's a draw</h2>}
+      {winner && winner !== 'draw' && (
+        <h2 className="winner">The winner is {winner}</h2>
+      )}
     </div>
   );
 }
--- tic-tac-toe-app/src/Components/Square/index.js
+++ tic-tac-toe-app/src/Components/Square/index.js
@@ -3,7 +3,9 @@ import './Square.css';
 function Square({ index, clickHandler, value }) {
   return (
     <div id={index} className="square" onClick={clickHandler}>
-      {value}
+      <p style={value === 'X' ? { color: '#3498db' } : { color: '#8e44ad' }}>
+        {value}
+      </p>
     </div>
   );
 }
